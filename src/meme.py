e78df7699c24459e8d19638be5e00675f6f24bfd
Kaiose31 <saurabhpahwa31@gmail.com>
2021-12-02 23:28:31 +0800

    usb

--- src/meme.py
+++ src/meme.py
@@ -3,10 +3,14 @@ from PIL import Image
 from io import BytesIO
 import random
 import pandas as pd
+import sys
+import usb.core
+import usb.util
 
 def get_insults():
     df = pd.read_csv('../data/impermium_verification_labels.csv')
     df.Comment.replace({r'[^\x00-\x7F]+':''}, regex=True, inplace=True)
+    df.Comment.replace('+',' ',inplace=True)
     return df.loc[df['Insult']==1]['Comment']
 
 def get_temps():
@@ -28,10 +32,18 @@ def get_meme():
         print("NO MEME FOR YOU")
         return None 
     else:
-        img = Image.open((BytesIO(r.content)))
-        img.show()
+        return Image.open((BytesIO(r.content)))
             
                     
 if __name__ == '__main__':
-    get_meme()
+    mode = sys.argv[1] if len(sys.argv) >1 else None 
+    if mode=='usb':
+        try:
+            dev = usb.core.find()
+            dev.set_configuration()
+            cfg = dev.get_active_configuration()
+        except ValueError as e:
+            print(f'USB Device Not Detected: {e}')    
+    else:
+        get_meme().show()
     
